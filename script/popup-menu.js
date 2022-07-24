170e1cc9c9ff4a33fe0e230ee2657b045d2e5d93
Yaroslav <MrStnr21@yandex.ru>
2022-07-24 14:38:46 +0300

    header-adaptive__done

---
+++ script/popup-menu.js
@@ -0,0 +1,29 @@
+// переменные для открытия/закрытия popup-menu
+
+const buttonPopupMenu = document.querySelector(".header__button-menu");
+const buttonClosePopupMenu = document.querySelector(
+  ".popup-menu__button-close"
+);
+const popupMenu = document.querySelector(".popup-menu");
+
+// функция открытия/закрытия popup-menu
+
+function openPopupMenu() {
+  popupMenu.classList.add("popup-menu_opened");
+}
+
+function ClosePopupMenu() {
+  popupMenu.classList.remove("popup-menu_opened");
+}
+
+function someFunc() {
+  const windowWidth = window.innerWidth;
+  if (windowWidth > 640) {
+    popupMenu.classList.remove("popup-menu_opened");
+  }
+}
+
+// Event listeners
+buttonPopupMenu.addEventListener("click", openPopupMenu);
+buttonClosePopupMenu.addEventListener("click", ClosePopupMenu);
+window.addEventListener("resize", someFunc)
